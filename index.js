424f08aa699d2ddd3c8ecb3237e014704cf1196d
Anshuls007 <anshulbhl.as@gmail.com>
2024-01-28 23:50:06 +0530

    style updated

--- index.js
+++ index.js
@@ -55,11 +55,6 @@ const artistsData = [
 ];
 
 const centeredDiv = document.getElementById("container");
-// centeredDiv.style.position = "absolute";
-// centeredDiv.style.top = "50%";
-// centeredDiv.style.left = "50%";
-// centeredDiv.style.transform = "translate(-50%, -50%)";
-// centeredDiv.style.border = "2px solid";
 artistsData.map((items) => {
   const cardDiv = document.createElement("div");
   cardDiv.className = "yourDesiredId";
@@ -78,7 +73,7 @@ artistsData.map((items) => {
   profileImage.src = profile;
   cardDiv1.appendChild(profileImage);
 
-  const artistName = document.createElement("h3");
+  const artistName = document.createElement("p");
   const nameData = document.createTextNode(`${items.name}`);
   artistName.className = "card-name";
   artistName.appendChild(nameData);
@@ -86,21 +81,19 @@ artistsData.map((items) => {
   
   cardDiv.appendChild(cardDiv1);
 
-  const artistdescription = document.createElement("p");
-  const descriptionData = document.createTextNode(`${items.description}`);
-  artistName.className = "card-description";
+  const artistdescription = document.createElement("i");
+  const descriptionData = document.createTextNode(`"${items.description}"`);
+  artistdescription.className = "card-description";
   artistdescription.appendChild(descriptionData);
   cardDiv2.appendChild(artistdescription);
 
   const artistBirth = document.createElement("p");
   const birthData = document.createTextNode(`BirthYear: ${items.birthYear}`);
-  artistName.className = "card-birth";
   artistBirth.appendChild(birthData);
   cardDiv2.appendChild(artistBirth);
 
   const artistCountry = document.createElement("p");
   const countryData = document.createTextNode(`Country: ${items.country}`);
-  artistName.className = "card-country";
   artistCountry.appendChild(countryData);
   cardDiv2.appendChild(artistCountry);
 
